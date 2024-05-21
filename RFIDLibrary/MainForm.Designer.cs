a5fbd34c3df7157bbb5301e451f2a9864e6ed6c1
AnteAndrijasevic <49294258+AnteAndrijasevic@users.noreply.github.com>
2024-05-22 00:14:58 +0200

    Add .gitignore file to exclude Visual Studio temporary files

---
+++ RFIDLibrary/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RFIDLibrary
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(12, 42);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(344, 20);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(12, 393);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(161, 45);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(194, 393);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(162, 45);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Location = new System.Drawing.Point(362, 42);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.Size = new System.Drawing.Size(426, 396);
+            this.dataGridViewBooks.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewBooks);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "MainForm";
+            this.Text = "RFIDLibrary";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridViewBooks;
+    }
+}
+
---
+++ RFIDLibrary/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.Sqlite;
+using System.Windows.Forms;
+using Impinj.OctaneSdk;
+
+namespace RFIDLibrary
+{
+    public partial class MainForm : Form
+    {
+        private ImpinjReader reader;
+        private string connectionString = "Data Source=library.db";
+        private string scannedRfidTag;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            using (var conn = new SqliteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "CREATE TABLE IF NOT EXISTS Books (Id INTEGER PRIMARY KEY, Title TEXT, RfidTag TEXT)";
+                var command = new SqliteCommand(sql, conn);
+                command.ExecuteNonQuery();
+            }
+            LoadBooks();
+        }
+
+        private void LoadBooks()
+        {
+            using (var conn = new SqliteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Books";
+                var command = new SqliteCommand(sql, conn);
+                using (var reader = command.ExecuteReader())
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dataGridViewBooks.DataSource = dt;
+                }
+            }
+        }
+
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            reader = new ImpinjReader();
+            try
+            {
+                reader.Connect("hostname"); // Zamijeni "hostname" s IP adresom ili imenom čitača
+                Settings settings = reader.QueryDefaultSettings();
+                settings.Report.IncludeAntennaPortNumber = true;
+                settings.Report.IncludeFirstSeenTime = true;
+                settings.Report.IncludePeakRssi = true;
+                settings.Report.IncludeSeenCount = true;
+
+                reader.ApplySettings(settings);
+                reader.TagsReported += OnTagsReported;
+                reader.Start();
+            }
+            catch (OctaneSdkException ex)
+            {
+                MessageBox.Show("Error connecting to reader: " + ex.Message);
+            }
+        }
+
+        private void OnTagsReported(ImpinjReader sender, TagReport report)
+        {
+            foreach (Tag tag in report)
+            {
+                scannedRfidTag = tag.Epc.ToString();
+                // Prikazivanje pronađenih tagova u MessageBox
+                MessageBox.Show($"RFID Tag: {scannedRfidTag}");
+            }
+            reader.Stop();
+            reader.Disconnect();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTitle.Text) && !string.IsNullOrEmpty(scannedRfidTag))
+            {
+                using (var conn = new SqliteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "INSERT INTO Books (Title, RfidTag) VALUES (@Title, @RfidTag)";
+                    var command = new SqliteCommand(sql, conn);
+                    command.Parameters.AddWithValue("@Title", txtTitle.Text);
+                    command.Parameters.AddWithValue("@RfidTag", scannedRfidTag);
+                    command.ExecuteNonQuery();
+                }
+                LoadBooks();
+            }
+            else
+            {
+                MessageBox.Show("Unesi naslov knjige i skeniraj RFID tag.");
+            }
+        }
+    }
+}
